35a4b8cc9221e74e2e9c7f86f55701b19c7695aa
FeRla17X <ucha2bokeria@mail.com>
2021-01-08 15:34:48 -0800

    1st

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  commands = [
+  {
+    time:"12:22",
+    txt:"Bless kengoorus Odin",
+    success:0,
+    permLvl:0
+  },
+  {
+    time:"12:22",
+    txt:"Bless kengoorus Odin",
+    success:0,
+    permLvl:0
+  },
+  {
+    time:"12:22",
+    txt:"Bless kengoorus Odin",
+    success:0,
+    permLvl:0
+  }
+  ];
   title = 'terminal';
 }
